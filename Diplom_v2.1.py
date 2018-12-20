3845dfd93f06333ebf374f857a7c17d83356b175
YakovFat <44905135+YakovFat@users.noreply.github.com>
2018-12-20 21:52:58 +0300

    gotovo

--- Diplom_v2.1.py
+++ Diplom_v2.1.py
@@ -1,5 +1,6 @@
-from cla import User
+from class_user import User
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     user = User('eshmargunov')
     print(user.id_unique_groups())
+# 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
--- class_user.py
+++ class_user.py
@@ -4,8 +4,14 @@ import sys
 import json
 from pprint import pprint
 
-print('Введите токен:')  # 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
+print('Введите токен:')
 token = input()
+
+default_params = {
+    'access_token': token,
+    'v': '5.92'
+}
+https = 'https://api.vk.com/method/'
 
 
 class User:
@@ -15,12 +21,9 @@ class User:
         if str(user_id).isdigit():
             self.user_id = user_id
         else:
-            params = {
-                'user_ids': user_id,
-                'access_token': token,
-                'v': '5.92'
-            }
-            response = requests.get('https://api.vk.com/method/users.get', params)
+            params = default_params.copy()
+            params.update({'user_ids': user_id})
+            response = requests.get(https + 'users.get', params)
             user_data = response.json()
             try:
                 self.user_id = user_data['response'][0]['id']
@@ -28,41 +31,25 @@ class User:
                 sys.exit('Данного пользователя не существует')
 
     def get_groups(self):
-        params = {
-            'user_id': self.user_id,
-            'extended': 1,
-            'fields': 'members_count',
-            'access_token': token,
-            'v': '5.92'
-        }
-
-        response = requests.get('https://api.vk.com/method/groups.get',
-                                params)
+        params = default_params.copy()
+        params.update({'user_id': self.user_id, 'extended': 1, 'fields': 'members_count'})
+        response = requests.get(https + 'groups.get', params)
         print('.')
         return response.json()
 
     def get_friends(self):
-        params = {
-            'user_id': self.user_id,
-            'access_token': token,
-            'v': '5.92'
-        }
+        params = default_params.copy()
+        params.update({'user_id': self.user_id})
 
-        response = requests.get('https://api.vk.com/method/friends.get',
-                                params)
+        response = requests.get(https + 'friends.get', params)
         print('.')
         return response.json()
 
     def groups_get_by_id(self, group_id):
+        params = default_params.copy()
+        params.update({'group_ids': group_id, 'fields': 'members_count'})
 
-        params = {
-            'group_ids': group_id,
-            'fields': 'members_count',
-            'access_token': token,
-            'v': '5.92'
-        }
-        response = requests.get('https://api.vk.com/method/groups.getById',
-                                params)
+        response = requests.get(https + 'groups.getById', params)
         print('.')
         return response.json()
 
@@ -78,13 +65,12 @@ class User:
             return 'error_code = 6'
         elif 'error' in user_friends and 'error_code' in user_friends['error'] and user_friends['error'][
             'error_code'] == 18:
-            time.sleep(1)
             print('Пользователь id{} был удален или забанен'.format(self.user_id))
             return 'error_code = 18'
         elif 'error' in user_friends and 'error_code' in user_friends['error'] and user_friends['error'][
             'error_code'] == 30:
-            print('Этот профиль приватный')
-            sys.exit()
+            print('Этот профиль id{} приватный'.format(self.user_id))
+            return 'error_code = 30'
         else:
             for i in user_friends['response']['items']:
                 id_groups.append(i['id'])
@@ -104,25 +90,28 @@ class User:
         return id_groups_user
 
     def id_unique_groups(self):
-        final_list = []
-        unique_groups = User(self.user_id).mutual()
-        for a in unique_groups:
-            info = User(self.user_id).groups_get_by_id(a)
-            if 'error' in info:
-                unique_groups.append(a)
-                time.sleep(1)
-            else:
-                for i in info['response']:
-                    if 'error' in i:
-                        unique_groups.append(a)
-                        time.sleep(1)
-                    else:
-                        json_group = {
-                            'name': i['name'],
-                            'gid': i['id'],
-                            'members_count': i['members_count']
-                        }
-                        final_list.append(json_group)
-        with open("newsafr2.json", "w", encoding='utf_8_sig') as datafile:
-            json.dump(final_list, datafile, ensure_ascii=False, indent=4)
-        pprint(final_list)
+        try:
+            final_list = []
+            unique_groups = User(self.user_id).mutual()
+            for a in unique_groups:
+                info = User(self.user_id).groups_get_by_id(a)
+                if 'error' in info:
+                    unique_groups.append(a)
+                    time.sleep(1)
+                else:
+                    for i in info['response']:
+                        if 'error' in i:
+                            unique_groups.append(a)
+                            time.sleep(1)
+                        else:
+                            json_group = {
+                                'name': i['name'],
+                                'gid': i['id'],
+                                'members_count': i['members_count']
+                            }
+                            final_list.append(json_group)
+            with open("unique_groups.json", "w", encoding='utf_8_sig') as datafile:
+                json.dump(final_list, datafile, ensure_ascii=False, indent=4)
+            pprint(final_list)
+        except KeyError:
+            pass
